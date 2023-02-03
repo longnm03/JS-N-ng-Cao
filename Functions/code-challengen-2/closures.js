34f73aa41502a84a535f1ff08ccc0f42217eba2e
longnm03 <105411215+longnm03@users.noreply.github.com>
2023-02-03 23:02:44 +0700

    Closures

---
+++ Functions/code-challengen-2/closures.js
@@ -0,0 +1,12 @@
+const secureBooking = function () {
+  let passengerCount = 0;
+  return function () {
+    passengerCount++;
+    console.log(`${passengerCount} passengers`);
+  };
+};
+const booker = secureBooking();
+booker();
+booker();
+booker();
+console.dir(booker);
