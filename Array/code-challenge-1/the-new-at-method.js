6df8c350a79cd27de0838979881e6875164c079b
longnm03 <105411215+longnm03@users.noreply.github.com>
2023-02-07 21:18:53 +0700

    The new at Method

---
+++ Array/code-challenge-1/the-new-at-method.js
@@ -0,0 +1,9 @@
+const arr = [23, 11, 64];
+console.log(arr[0]); // 23
+console.log(arr.at(0)); // 23
+// getting last array element
+console.log(arr[arr.length - 1]); // 64
+console.log(arr.slice(-1)[0]); // 64
+console.log(arr.at(-1)); // 64
+console.log("jonas".at(0)); // j
+console.log("jonas".at(-1)); // s
